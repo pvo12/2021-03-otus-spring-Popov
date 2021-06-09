dbacf031b43877aaa4c4d239d2700a2084bc0c3a
vpopov <vpopov@diasoft.ru>
2021-06-09 12:07:59 +0700

    spring11

--- spring-11/src/main/java/ru/otus/spring/service/BookCommentServiceImpl.java
+++ spring-11/src/main/java/ru/otus/spring/service/BookCommentServiceImpl.java
@@ -2,6 +2,7 @@ package ru.otus.spring.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ru.otus.spring.domain.BookComment;
 import ru.otus.spring.repositories.BookCommentRepository;
 
@@ -13,6 +14,7 @@ public class BookCommentServiceImpl implements BookCommentService{
     private final BookCommentRepository repository;
 
     @Override
+    @Transactional(readOnly = true)
     public List<BookComment> getAll() {
         return repository.findAll();
     }
